c699c487c34412b8241cc10837f3b312795a7fae
Umang.patel@auronia.io-at-591922378350 <2LrJ8HX3k17WvjVpM0hPe4IW4Io2Fo8KSyCUrMSZzxQ=>
2020-05-09 19:28:18 +0530

    Added firebase database integration in recyclerview

--- app/src/main/java/gnu/uvpce/androidwebinar/MainActivity.java
+++ app/src/main/java/gnu/uvpce/androidwebinar/MainActivity.java
@@ -1,5 +1,6 @@
 package gnu.uvpce.androidwebinar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -7,6 +8,13 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
@@ -15,8 +23,11 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView.Adapter adapter;
     private RecyclerView.LayoutManager layoutManager;
     private RecyclerView recyclerView;
-    private ArrayList<Model> data;
+    private ArrayList<Model> data = new ArrayList<>();
     private Context mContext = this;
+
+    // Creating DatabaseReference.
+    DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,16 +41,47 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(layoutManager);
         recyclerView.setItemAnimator(new DefaultItemAnimator());
 
-        data = new ArrayList<Model>();
+        // Setting up Firebase database path "android_versions" in databaseReference.
+        databaseReference = FirebaseDatabase.getInstance().getReference("android_versions");
+        // Adding Add Value Event Listener to databaseReference.
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                for (DataSnapshot postSnapshot : snapshot.getChildren()) {
+
+                    //getting the Model class data with "name, version, image"
+                    Model model = postSnapshot.getValue(Model.class);
+
+                    //adding the single model class into the list
+                    data.add(model);
+
+                    adapter = new RecyclerViewAdapter(data, mContext);
+                    recyclerView.setAdapter(adapter);
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                //Show the error message in a toast
+                Toast.makeText(mContext, "Error: " + databaseError.getMessage(), Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+
+
+   /*     data = new ArrayList<Model>();
         for (int i = 0; i < Constants.nameArray.length; i++) {
             data.add(new Model(
                     Constants.nameArray[i],
                     Constants.versionArray[i],
                     Constants.drawableArray[i]
             ));
-        }
+        }*/
 
-        adapter = new RecyclerViewAdapter(data,mContext);
-        recyclerView.setAdapter(adapter);
+
     }
 }
